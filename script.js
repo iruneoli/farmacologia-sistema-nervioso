5933572c82bcdccecd9a57bbe8b0eac7c2ad04ed
iruneoli <faschatgpt@gmail.com>
2026-03-14 14:10:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("La web funciona");
+function actividad1(){
+
+document.getElementById("contenido").innerHTML =
+"<h2>Actividad 1: Definiciones de subgrupos</h2>";
+
+}
+
+function actividad2(){
+
+document.getElementById("contenido").innerHTML =
+"<h2>Actividad 2: Clasificar principios activos</h2>";
+
+}
+
+function actividad3(){
+
+document.getElementById("contenido").innerHTML =
+"<h2>Actividad 3: Subgrupo / efectos adversos</h2>";
+
+}
+
+function actividad4(){
+
+document.getElementById("contenido").innerHTML =
+"<h2>Actividad 4: EF ↔ principio activo</h2>";
+
+}
+
+function actividad5(){
+
+document.getElementById("contenido").innerHTML =
+"<h2>Actividad 5: Tipo test</h2>";
+
+}
